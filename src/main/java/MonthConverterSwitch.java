83b34c8cc6266769069505280598cbfe44c14814
regina wells <jenajuan@hotmail.com>
2019-07-03 09:43:58 -0500

    first commit

---
+++ src/main/java/MonthConverterSwitch.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MonthConverterSwitch {
+    public static void Main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("Enter a number between 1 and 12.");
+
+
+        if (month>=1 && < 12 );{
+            System.out.println("Jan");
+        } else {
+            System.out.println("You must enter a number between 1 and 12");
+        }
+    }
+}
+
+
