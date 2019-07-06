81c3dd9c90d023c49193322092b84bf78927d04d
regina wells <jenajuan@hotmail.com>
2019-07-06 00:02:55 -0500

    initial commit

--- src/main/java/MonthConverterSwitch.java
+++ src/main/java/MonthConverterSwitch.java
@@ -2,17 +2,27 @@ import java.util.Scanner;
 
 public class MonthConverterSwitch {
     public static void Main(String[] args) {
+
+//        String month;
+//        month = (x);
+
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String month = scanner.nextLine();
         System.out.println("Enter a number between 1 and 12.");
 
 
-        if (month>=1 && < 12 );{
-            System.out.println("Jan");
-        } else {
-            System.out.println("You must enter a number between 1 and 12");
+//        if(x >= 1 && x <= 12);
+//            System.out.println("Jan");
+//            System.out.println("You must enter a number between 1 and 12");
         }
-    }
 }
+//int num1 = 4
+//int num2 = 5;
+//
+//if(num1 == num2){
+//    System.out.println(“These numbers are equal”);
+//}else {
+//    System.out.println(“These numbers are not equal”);
+//}
 
 
